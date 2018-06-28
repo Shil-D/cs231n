3758c15ff27ac3ddc819229c22ace41daa4f26aa
Шиляев Денис Васильевич <SBT-Shilyaev-DV@sigma.sbrf.ru>
2018-06-28 11:39:45 +0300

    Q3A1

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -29,7 +29,29 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_training = X.shape[0]
+  C = W.shape[1]
+
+  for i in range(num_training):
+    scores = X[i].dot(W)
+    exp_scores = np.exp(scores)
+    probabilities = exp_scores / exp_scores.sum()
+    correct_class_prob = probabilities[y[i]]
+    loss += -np.log(correct_class_prob)
+
+    dLdfyi = -1
+    dfyidWyi = X[i] * dLdfyi
+    dW[:, y[i]] += dfyidWyi
+    for j in range(C):
+      dLdefj = 1 / exp_scores.sum()
+      defjdWj = exp_scores[j] * X[i] * dLdefj
+      dW[:, j] += defjdWj
+
+
+  loss /= num_training
+  loss += reg * np.sum(W * W)
+  dW /= num_training
+  dW += reg * 2 * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -53,7 +75,25 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_training = X.shape[0]
+
+  scores = X.dot(W)
+  exp_scores = np.exp(scores)
+  sum = exp_scores.sum(axis=1)
+  probabilities = (exp_scores.T / sum).T
+
+  loss += np.sum(-np.log(probabilities[np.arange(num_training), y]))
+  loss /= num_training
+  loss += reg * np.sum(W * W)
+
+  # each i-th element of the column of the indicator matrix is coefficient of x_i
+  # in the sum of all training examples
+  indicator = probabilities.copy()
+  indicator[np.arange(num_training), y] -= 1
+
+  dW += X.T.dot(indicator)
+  dW /= num_training
+  dW += reg * 2 * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
